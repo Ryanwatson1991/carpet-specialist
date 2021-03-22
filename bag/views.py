40b5909884838f494cc69754638cd83fa86ba125
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-22 15:55:23 +0000

    Different notifications display in toast depending on action taken

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of product to the bag"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     carpet_width = 0
@@ -28,15 +28,20 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if carpet_area in bag[item_id]['item_measurements'].keys():
                 bag[item_id]['item_measurements'][carpet_area] += quantity
+                messages.success(request, f'Updated { product.name } quantity to {bag[item_id]["items_by_size"]}')
+                # Note - this isn't working properly, need to fix it so that it updated quantity if two of the same size carpets are added to bag
             else:
                 bag[item_id]['item_measurements'][carpet_area] = quantity
+                messages.success(request, f'Added { product.name } to your bag')
         else:
             bag[item_id] = {'item_measurements': {
                 carpet_area : quantity
                 }}
+            messages.success(request, f'Added { product.name } to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated { product.name } quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added { product.name } to your bag')
@@ -48,18 +53,25 @@ def add_to_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        carpet_area = None
+        if 'carpet_area' in request.POST:
+            carpet_area = request.POST['carpet_area']
+        bag = request.session.get('bag', {})
 
-    carpet_area = None
-    if 'carpet_area' in request.POST:
-        carpet_area = request.POST['carpet_area']
-    bag = request.session.get('bag', {})
-
-    if carpet_area:
-        del bag[item_id]['item_measurements'][carpet_area]
-        if not bag[item_id]['item_measurements']:
+        if carpet_area:
+            del bag[item_id]['item_measurements'][carpet_area]
+            if not bag[item_id]['item_measurements']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed { product.name } { carpet_area }M² from your bag')
+        else:
             bag.pop(item_id)
-    else:
-        bag.pop(item_id)
+            messages.success(request, f'Removed { product.name } from your bag')
 
-    request.session['bag'] = bag
-    return HttpResponse(status=200)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        messages.error(request, f'Error removing item')
+        return HttpResponse(status=500) 
