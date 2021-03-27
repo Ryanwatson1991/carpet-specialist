c3049ed1403c6d2c23b48bb1e6794eed16e345ec
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-27 12:17:19 +0000

    Added profile form and updated view

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -21,7 +21,7 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs): 
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update user profile
     """
