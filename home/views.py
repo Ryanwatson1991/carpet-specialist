8079a06cd07d4cdd5bac00d1aad8d6caa34d87bb
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-29 05:51:18 +0000

    Added contact page, form and view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """View that returns index page"""
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """View that returns contact page - have put this in home app as it's something pretty small that I didn't want to create a new app for"""
+
+    return render(request, 'home/contact.html')
