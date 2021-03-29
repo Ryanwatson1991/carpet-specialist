f77195e9351984701b9b909371701eb837f63ec9
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-29 07:17:21 +0000

    Set up postgres database

--- carpet_specialist/settings.py
+++ carpet_specialist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,8 +124,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+   }
+
 
 
 # Password validation
