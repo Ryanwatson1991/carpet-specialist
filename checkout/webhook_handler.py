380e71137e85a1be7ead83d882ea8ec49ba5da09
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-29 18:47:09 +0000

    updated OrderLineItem dictionary in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -136,9 +136,9 @@ class StripeWH_Handler:
                                 product=product,
                                 quantity=quantity,
                                 carpet_area=carpet_area,
-                                carpet_length=carpet_length,
-                                carpet_width=carpet_width,
-                                carpet_colour=carpet_colour,
+                                carpet_length=item_data['carpet_details']['carpet_length'],
+                                carpet_width=item_data['carpet_details']['carpet_width'],
+                                carpet_colour=item_data['carpet_details']['carpet_colour'],
                             )
                             order_line_item.save()
             except Exception as e:
