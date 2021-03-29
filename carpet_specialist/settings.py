421f7ecdab0a2c23954f70ed9fc6a70dc51fe75c
Ryanwatson1991 <Watson.ryan.lee@outlook.com>
2021-03-29 07:59:15 +0000

    Fixed typo in 'ALLOWED_HOSTS settings.py

--- carpet_specialist/settings.py
+++ carpet_specialist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['carpet_specialist.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['carpet-specialist.herokuapp.com', 'localhost']
 
 
 # Application definition
